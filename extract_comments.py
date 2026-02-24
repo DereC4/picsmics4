bda0b009165bcae7e7c5a4beadaafafc76ea699e
Derek <derex1987@gmail.com>
2026-02-23 20:53:38 -0600

    output path

--- extract_comments.py
+++ extract_comments.py
@@ -2,13 +2,14 @@ import json
 from yt_dlp import YoutubeDL
 
 url = "https://www.youtube.com/watch?v=lW7vWfRI5oI"
+output_path = "docs/comments.json"
 
 with YoutubeDL({"skip_download": True, "getcomments": True}) as derexXD:
     info = derexXD.extract_info(url, download=False)
 
 comments = sorted(info.get("comments", []), key=lambda x: x.get("timestamp", 0))
 
-with open("docs/comments.json", "w") as file:
+with open(output_path, "w") as file:
     json.dump(comments, file, indent=2)
 
 print(f"Done\nExtracted {len(comments)} comments from el video (sorted oldest to newest)")
